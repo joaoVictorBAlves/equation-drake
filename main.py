9cf820db21ebed5e48118c1643a0c636bec59768
João Victor <joaovba2002@alu.ufc.br>
2022-12-06 23:08:58 -0300

    Init with pygame

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from Simulator import Simulator
+import pygame
+from pygame.locals import *
+from sys import exit
 
-if __name__ == "__main__":
-    simulator = Simulator()
-    simulator.start_world()
-    print(simulator.drake_equation())
+pygame.init()
+width = 400
+height = 400
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Simulador Equação de Drake')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+    pygame.draw.rect(window, (255, 0, 0),
+                     (width/4, height/4, width/2, height/2))
+    pygame.draw.circle(window, (0, 0, 120), (width/2, height/2), width/4)
+    pygame.display.update()
