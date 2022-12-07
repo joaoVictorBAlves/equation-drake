93361f6e5631f6c23947f8d7690eace29f2a5a0f
João Victor <joaovba2002@alu.ufc.br>
2022-12-06 23:10:43 -0300

    Adjusts pygame

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ window = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Simulador Equação de Drake')
 
 while True:
+    """ Events """
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-    pygame.draw.rect(window, (255, 0, 0),
-                     (width/4, height/4, width/2, height/2))
+            
+    """ Draw """
+    pygame.draw.rect(window, (255, 0, 0), (width/4, height/4, width/2, height/2))
     pygame.draw.circle(window, (0, 0, 120), (width/2, height/2), width/4)
     pygame.display.update()
